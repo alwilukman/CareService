c4d823d62bd2a4d316a7780eed7bf8e3ffff13f0
Arkan <arkanyeb@gmail.com>
2022-07-09 09:20:45 +0700

    Maps

--- app/src/main/java/Careservice/pa/MainActivity.kt
+++ app/src/main/java/Careservice/pa/MainActivity.kt
@@ -1,11 +1,26 @@
 package Careservice.pa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_maps) {
+            startActivity(Intent(this, MapsActivity::class.java))
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
